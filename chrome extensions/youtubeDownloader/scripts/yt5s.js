38a6115cee39acd6634dc911cc0a675b991a4646
Deepesh Gupta <34032722+PrinceDeepu@users.noreply.github.com>
2022-05-28 12:32:54 +0530

    added open youtube link button in yt5s

--- chrome extensions/youtubeDownloader/scripts/yt5s.js
+++ chrome extensions/youtubeDownloader/scripts/yt5s.js
@@ -1,26 +1,31 @@
-
 async function yt5s() {
-    if(window.location.host.indexOf("yt5s.com") !== -1) {
-
+    if (window.location.host.indexOf("yt5s.com") !== -1) {
         const url = new URL(window.location.href);
-        if(url.searchParams.get('utmSource') === 'deepeshdg') {
-
-            let interval = setInterval( async () => {
-                await myObserver("#search-form button", node => node.click() )
+        if (url.searchParams.get("utmSource") === "deepeshdg") {
+            let interval = setInterval(async () => {
+                await myObserver("#search-form button", (node) => node.click());
             }, 300000);
 
-            await myObserver('#formatSelect', node => {
+            await myObserver("#search-form", (node) => {
+                const btn = document.createElement("a");
+                btn.classList.add("btn-red");
+                btn.target = "_blank";
+                btn.textContent = "Open";
 
-                if(url.searchParams.get("downloadType") === 'audio') 
+                btn.href = url.searchParams.get("q");
+                node.append(btn);
+            });
+
+            await myObserver("#formatSelect", (node) => {
+                if (url.searchParams.get("downloadType") === "audio")
                     node.selectedIndex = node.options.length - 1;
-                else
-                    node.selectedIndex = 0;
-                
+                else node.selectedIndex = 0;
+
                 clearInterval(interval);
             });
 
-            await myObserver('#btn-action', node => node.click() );
-            await myObserver('#asuccess:not(.hidden)', node => node.click() );
+            await myObserver("#btn-action", (node) => node.click());
+            await myObserver("#asuccess:not(.hidden)", (node) => node.click());
         }
     }
 }
